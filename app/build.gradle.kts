7fa6369a75e642ce3336c7476c19e8405f4d4ee7
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-11-12 03:47:49 +0700

    last commit for Menampilkan data kedalam Listview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "id.ac.polbeng.edisonrizal.viewdata"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "id.ac.polbeng.edisonrizal.viewdata"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- app/src/main/java/id/ac/polbeng/edisonrizal/viewdata/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/edisonrizal/viewdata/MainActivity.kt
@@ -1,20 +1,24 @@
 package id.ac.polbeng.edisonrizal.viewdata
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.edisonrizal.viewdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnViewList.setOnClickListener{
+            startActivity(
+                Intent(applicationContext,
+                ListviewActivity::class.java)
+            )
         }
     }
 }
